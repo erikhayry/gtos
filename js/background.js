e72ef0b3e8ab6db6ec91390367ef6af72bb48384
Erik Portin <erik.portin@net-a-porter.comn>
2013-10-09 17:16:12 +0100

    moved funtionality to content.js

--- js/background.js
+++ js/background.js
@@ -5,7 +5,6 @@
     
     //if on grooveshark load content.js
     if (tab.url.indexOf('grooveshark.com') > -1) {
-      
       //save url to localstorage
       localStorage['tabUrl'] = tab.url;
       
@@ -22,15 +21,6 @@
   //listen for any changes to the URL of any tab.
   chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
-  chrome.extension.onMessage.addListener(
-      function(request, sender, sendResponse){
-          if(request.type == "songData"){
-              console.log('recieving')
-              /* The type of message has been identified as the variable for our popup, let's save it to localStorage */
-              localStorage["songData"] = request.query;
-          }
-      }
-  );
 })();
 
 
--- js/content.js
+++ js/content.js
@@ -1,51 +1,146 @@
 (function(){
+	var query = '';
 
-	function getQuery(){
-		var song = document.querySelectorAll('#now-playing-metadata .song')[0], 
-			artist = document.querySelectorAll('#now-playing-metadata .artist')[0],
-			query = '';
+	var _getQuery = function(){
+
+		var newQuery = '',
+
+			//check if song is currently playing
+			song = document.querySelectorAll('#now-playing-metadata .song')[0], 
+			artist = document.querySelectorAll('#now-playing-metadata .artist')[0];
 			
-			if(song && song.innerText) query += song.innerText + ' ';
-			if(artist && artist.innerText) query += artist.innerText;
-			return query; 
-	}
+			//Pixies 
+			if(artist && artist.innerText) newQuery += artist.innerText.split(' ').join('+') + '+'; //Pixies+
+			//Trompe le Monde Pixies 
+			if(song && song.innerText) newQuery += song.innerText.split(' ').join('+'); //Pixies+Trompe+le+Monde
 
-	var url = window.location.href,
-		i = url.indexOf('/s/'),
-		n = url.lastIndexOf('/'),
-		urlQuery = url.substring(i + 3, n);
-	
-	if(urlQuery.indexOf('grooveshark') < 0) {
-		localStorage['tabUrl'] = urlQuery
-	}
 
-	else localStorage['tabUrl'] = '';
-	
-	localStorage['songData'] = getQuery();
+			
+			//no song is playing. Try to get data from url instead
+			if(!newQuery){
+				/*
+					search urls : http://grooveshark.com/#!/search?q=pj+thom,
+								  http://grooveshark.com/#!/search/song?q=pj+harvey+thom+yorke
 
-	setInterval(function(){
-		localStorage['songData'] = getQuery();
-	}, 3000);
+					track url : http://grooveshark.com/s/Where+Is+My+Mind/3SG5Ua?src=5
 
-	/*
-		Assuming Groovesharks play button got a id of 'play-pause' and a class of 'playing' when playing
-	 */
+					profile url : http://grooveshark.com/#!/profile/Pixies/22180913
 
-	var paused = false,
-		loc = window.location.host;
+					album url : http://grooveshark.com/#!/album/Trompe+Le+Monde/1261426
 
-		var interval = setInterval(function(){
-			console.log('checking')
-			var playPauseBtn = document.getElementById('play-pause');
-			if(!paused && playPauseBtn && playPauseBtn.className.indexOf('playing') > -1){
-				playPauseBtn.click();
+				 */
+
+				var url = window.location.href,
+					type = '';
+
+				// /#!/search?q=pj+thom	
+				if(url.indexOf('#!/search') > -1){
+					var queries = url.slice(url.indexOf('?') + 1).split('&'); //['q=pj+thom']
+					for (var i = 0; i < queries.length; i++) {
+					  	var indexOfQ = queries[i].indexOf('q=');
+						if(indexOfQ > -1){ //'q=pj+thom'
+							newQuery = queries[i].substring(indexOfQ + 2); //pj+thom
+					  	}  
+					};					 
+				}
+
+				// /s/Where+Is+My+Mind/3SG5Ua?src=5 or /#!/profile/Pixies/22180913 or /#!/album/Trompe+Le+Monde/1261426
+				else if((url.indexOf('/s/') > -1) || (url.indexOf('#!/profile/') > -1) || (url.indexOf('#!/album/') > -1)){
+					if(url.indexOf('/s/') > -1) var hash = '/s/';
+					else if(url.indexOf('#!/profile/') > -1) var hash = '#!/profile/';
+					else var hash = '#!/album/'; 	
+
+					var indexOfHash = url.indexOf(hash),
+						indexOfLastSlash = url.lastIndexOf('/');		
+					
+					newQuery = url.substring(indexOfHash + hash.length, indexOfLastSlash); // Where+Is+My+Mind or Pixies or Trompe+Le+Monde
+				}
 			}
-			localStorage['songData'] = getQuery();
-			
-			chrome.extension.sendMessage({
-			    type: "songData", 
-			    query: getQuery()
-			});
 
+			return newQuery; 
+	},
+
+	_stringToNode = function(tmplString){
+		var div = document.createElement('div');
+		div.innerHTML = tmplString;
+		return div.childNodes[0];
+	};
+
+
+
+	var popUpTmpl = _stringToNode(
+								'<div ng-app="sToG" ng-csp ng-controller="messageHolder">' + 
+									'{{message}}' +
+								'</div>'
+								);
+
+	var init = function(){
+
+		document.body.appendChild(popUpTmpl);
+
+	}			
+
+
+	init();
+
+	var App = angular.module('sToG', []);
+
+/*	App.factory('spotifyFactory', function(){
+		var factory = {},
+
+		_getData = function(){
+			$http.get('http://ws.spotify.com/search/1/track.json?q=' + query).success(function(data) {
+			});	
+		}
+
+
+
+		return factory;
+
+	});*/
+
+	App.controller('messageHolder', function($scope, $http){
+		var interval = setInterval(function(){
+			var newQuery = _getQuery();
+			if(newQuery !== query){
+				query = newQuery;
+				console.log(query)
+				$http.get('http://ws.spotify.com/search/1/track.json?q=' + query).success(function(data) {
+					/*
+						info: Object
+							limit: 100
+							num_results: 504
+							offset: 0
+							page: 1
+							query: "Pixies"
+							type: "track"
+						tracks: Array[100]
+							0: Object
+								album: Object
+								artists: Array[1]
+								external-ids: Array[1]
+								href: "spotify:track:5BP0oaQ1VhuaznT77CBXQp"
+								length: 231.931
+								name: "Where Is My Mind?"
+								popularity: "0.74"
+								track-number: "14"
+					 */
+					console.log(data);
+					
+				});
+				$scope.$apply(function(){
+					$scope.message = query;
+				})
+			}
 		}, 1000);
+	});
+
+	App.config(function($httpProvider){
+    	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	});
+
+	angular.bootstrap(popUpTmpl, ['sToG']);
+
+
+
 })();
